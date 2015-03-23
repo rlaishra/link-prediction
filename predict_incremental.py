6e812f7f350fef308cd8e4966b5cc6d6a59bdee9
Ricky <rickylaishram@gmail.com>
2015-03-23 16:44:02 -0400

    Save measures in db

--- predict_incremental.py
+++ predict_incremental.py
@@ -348,13 +348,15 @@ class MeasuresWeightAMIncremental(object):
 		self._decay_factor = decay_factor
 		self._default_link_weight = 1
 
-		self._common_neighbors 	= {}
-		self._jaccard_coefficient = {}
-		self._adamic_adar = {}
-		self._preferentail_attachment = {}
-		self._shortest_path = {}
+		self.common_neighbors 	= {}
+		self.jaccard_coefficient = {}
+		self.adamic_adar = {}
+		self.preferentail_attachment = {}
+		self.shortest_path = {}
+		self.katz_score = {}
+		self.rooted_page_rank = {}
 
-		self._db = NetworkDB()
+		#self._db = NetworkDB()
 
 		self._edges = []
 
@@ -363,8 +365,8 @@ class MeasuresWeightAMIncremental(object):
 				self._edges.append((self._nodes[i], self._nodes[j]))
 				self._edges.append((self._nodes[j], self._nodes[i]))
 
-				self._common_neighbors[(self._nodes[i], self._nodes[j])] = 0
-				self._common_neighbors[(self._nodes[j], self._nodes[i])] = 0
+				self.common_neighbors[(self._nodes[i], self._nodes[j])] = 0
+				self.common_neighbors[(self._nodes[j], self._nodes[i])] = 0
 
 	def cprint(self, v):
 		sys.stdout.write("\r%s" % v)
@@ -373,7 +375,7 @@ class MeasuresWeightAMIncremental(object):
 	# Call only once per decay time
 	def decay(self):
 		for edge in self._edges:
-			self._common_neighbors[edge] *= self._decay_factor 
+			self.common_neighbors[edge] *= self._decay_factor 
 
 	def _update_common_neighbor(self, u, v, link_exist=False):
 		graph = self._network.get_graph()
@@ -384,20 +386,20 @@ class MeasuresWeightAMIncremental(object):
 		for n in nodes1:
 			if n in self._nodes:
 				if link_exist and (n != u):
-					self._common_neighbors[(n,v)] += self._default_link_weight/2
+					self.common_neighbors[(n,v)] += self._default_link_weight/2
 				elif not link_exist:
-					self._common_neighbors[(n,v)] += (self._default_link_weight + graph[n][v]['weight'])/2
-				self._common_neighbors[(v,n)] = self._common_neighbors[(n,v)]
+					self.common_neighbors[(n,v)] += (self._default_link_weight + graph[n][v]['weight'])/2
+				self.common_neighbors[(v,n)] = self.common_neighbors[(n,v)]
 		
 		# Get all nodes that has u as in neighbor
 		nodes2 = graph[u].keys()
 		for n in nodes2:
 			if n in self._nodes:
 				if link_exist and (n != v):
-					self._common_neighbors[(u,n)] += self._default_link_weight/2
+					self.common_neighbors[(u,n)] += self._default_link_weight/2
 				elif not link_exist:
-					self._common_neighbors[(u,n)] += (self._default_link_weight + graph[u][n]['weight'])/2
-				self._common_neighbors[(n,u)] = self._common_neighbors[(u,n)]
+					self.common_neighbors[(u,n)] += (self._default_link_weight + graph[u][n]['weight'])/2
+				self.common_neighbors[(n,u)] = self.common_neighbors[(u,n)]
 
 	# New node is added. Update the common neighbor scores
 	def update(self, edge):
@@ -425,11 +427,16 @@ class Main(object):
 		#self.time_start, self.time_end = self.db.get_time_min_max()
 		self.time_start = 1422289905
 		self.delta_time = 3600
+		self.prediction_time = 72 	# Time in future to predict
 
 		self.network = graph.SocialNetwork(0.1, self.users_valid, weighted=True)
 		self.network.initialize_nodes(self.users_valid)
 
 		self.measures = MeasuresWeightAMIncremental(self.network, self.users_sample)
+		
+		self.dbconn = sqlite3.connect('db/features.db')
+		self.dbcursor = self.dbconn.cursor()
+
 
 	def cprint(self, v):
 		sys.stdout.write("\r%s" % v)
@@ -451,14 +458,33 @@ class Main(object):
 				ls.append((n1,n2))
 		return ls
 
+	def save_features_class(self, features, mtype, i, edges=None):
+		for edge in features:
+			val = (edge[0], edge[1], i, mtype, features[edge])
+			self.dbcursor.execute('INSERT INTO `features` (`from_node`, `to_node`, `time`, `type`, `value`) VALUES (?, ?, ?, ?, ?) ', val)
+			
+			if edges is not None:
+				class_val = 0
+				if edge in edges:
+					class_val = 1
+				val = (edge[0], edge[1], i, class_val)
+				self.dbcursor.execute('INSERT INTO `classes` (`from_node`, `to_node`, `time`, `value`) VALUES (?, ?, ?, ?) ', val)
+		self.dbconn.commit()
+
 	def run(self):
-		for i in xrange(1, 654):
-			self.cprint(str(int((i+1)*100/654))+'%'),
+		time_min = 1
+		time_max = 880
+		for i in xrange(1, time_max - self.prediction_time):
+			self.cprint(str(int((i+1)*100/802))+'%'),
 			edges = self.db.get_links(self.time_start+(i-1)*self.delta_time, self.time_start+i*self.delta_time, self.users_valid, True)
-			#print(edges)
 			self.measures.decay()
-			for e in self.convert_adjacency_to_list(edges):
+			edge_list = self.convert_adjacency_to_list(edges)
+			for e in edge_list:
 				self.measures.update(e)
+
+			#save measure
+			self.save_features_class(self.measures.common_neighbors, 'common_neighbors', i, edge_list)
+
 			self.network.add_edges(edges, 0.9)
 
 
